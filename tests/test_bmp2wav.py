Fixed B2W scaling issue TODO: adjust image functions to use generated filenames

tests/test_bmp2wav.py
@@ -19,9 +19,9 @@ class TestBmp2Wav(unittest.TestCase):
 
     def draw_diff(self, title, i, o, doAbs=True):
         if doAbs:
-            diff = np.abs(o) - np.abs(i)
+            diff = np.abs(i) - np.abs(o)
         else:
-            diff = o - i
+            diff = i - o
 
         fig = plt.figure()
         fig.suptitle("Array differences ({})".format(title))
@@ -30,11 +30,11 @@ class TestBmp2Wav(unittest.TestCase):
         plt.show(block=False)
 
 
-    def _test_bmp2wav_basic(self):
+    def test_bmp2wav_basic_low_fs(self):
         fs = 100.0
-        size = 16
-        overlapDec = 0.9375
-        l = 100
+        size = 4
+        overlapDec = 0.75
+        l = 10
         assert int(l / 2) == (l / 2)
 
         # Generate a test wav (sequence of +1.0, -1.0, +1.0, -1.0, ...)
@@ -48,29 +48,52 @@ class TestBmp2Wav(unittest.TestCase):
 
         # Do b2w
         out = fft.bmp2wav(fs, l, x, mask, size, overlapDec)
-        outMin = np.amin(out)
-        outMax = np.amax(out)
-        print("min(out) = {:+}".format(outMin))
-        print("max(out) = {:+}".format(outMax))
 
-        #if outMin < -1.0 or outMax > 1.0:
-        #    print("Resynthesized WAV samples are out-of-range; normalising...")
-        #    out2 = util.norm(out)
-        #else:
-        out2 = out
+        #fig = plt.figure()
+        #fig.suptitle("Original WAV vs reynthesized WAV (basic)")
+        #plt.subplot(2, 1, 1)
+        #plt.plot(np.arange(0, out.size, dtype="float32"), ar2)
+        #plt.grid(True)
+        #plt.subplot(2, 1, 2)
+        #plt.plot(np.arange(0, out.size, dtype="float32"), out)
+        #plt.grid(True)
+        #plt.show(block=False)
+        #self.draw_diff("basic", ar2, out)
 
-        print("Drawing graphs...")
-        fig = plt.figure()
-        fig.suptitle("Original WAV vs reynthesized WAV (basic)")
-        plt.subplot(2, 1, 1)
-        plt.plot(np.arange(0, out2.size, dtype="float32"), ar2)
-        plt.grid(True)
-        plt.subplot(2, 1, 2)
-        plt.plot(np.arange(0, out2.size, dtype="float32"), out2)
-        plt.grid(True)
-        plt.show(block=False)
+        self.assertTrue(np.allclose(ar2, out))
 
-        self.draw_diff("basic", ar2, out2)
+
+    def test_bmp2wav_basic_high_fs(self):
+        fs = 44100.0
+        size = 1024
+        overlapDec = 0.9375
+        l = 100000
+        assert int(l / 2) == (l / 2)
+
+        # Generate a test wav (sequence of +1.0, -1.0, +1.0, -1.0, ...)
+        ar = np.array([ 1.0, -1.0], dtype="float32")
+        ar2 = np.tile(ar, int(l / 2))
+        ab, an, x = fft.wav2bmp(fs, ar2, size, overlapDec, window=None)
+
+        # Generate a blank mask
+        mask = np.ndarray(ab.shape, dtype="float32")
+        mask[:, :] = 1.0
+
+        # Do b2w
+        out = fft.bmp2wav(fs, l, x, mask, size, overlapDec)
+
+        #fig = plt.figure()
+        #fig.suptitle("Original WAV vs reynthesized WAV (basic)")
+        #plt.subplot(2, 1, 1)
+        #plt.plot(np.arange(0, out.size, dtype="float32"), ar2)
+        #plt.grid(True)
+        #plt.subplot(2, 1, 2)
+        #plt.plot(np.arange(0, out.size, dtype="float32"), out)
+        #plt.grid(True)
+        #plt.show(block=False)
+        #self.draw_diff("basic", ar2, out)
+
+        self.assertTrue(np.allclose(ar2, out))
 
 
     def test_bmp2wav_square_2(self):
@@ -84,7 +107,7 @@ class TestBmp2Wav(unittest.TestCase):
         else:
             ar2 = ar
 
-        ab, an, x = fft.wav2bmp(fs, ar2, size, overlapDec)
+        ab, an, x = fft.wav2bmp(fs, ar2, size, overlapDec, window=None)
 
         # Generate a blank mask
         mask = np.ndarray(ab.shape, dtype="float32")
@@ -92,29 +115,19 @@ class TestBmp2Wav(unittest.TestCase):
 
         # Do b2w
         out = fft.bmp2wav(fs, l, x, mask, size, overlapDec)
-        outMin = np.amin(out)
-        outMax = np.amax(out)
-        print("min(out) = {:+}".format(outMin))
-        print("max(out) = {:+}".format(outMax))
 
-        #if outMin < -1.0 or outMax > 1.0:
-        #    print("Resynthesized WAV samples are out-of-range; normalising...")
-        #    out2 = util.norm(out)
-        #else:
-        out2 = out
+        #fig = plt.figure()
+        #fig.suptitle("Original WAV vs reynthesized WAV (basic)")
+        #plt.subplot(2, 1, 1)
+        #plt.plot(np.arange(0, out.size, dtype="float32"), ar2)
+        #plt.grid(True)
+        #plt.subplot(2, 1, 2)
+        #plt.plot(np.arange(0, out.size, dtype="float32"), out)
+        #plt.grid(True)
+        #plt.show(block=False)
+        #self.draw_diff("square_2", ar2, out)
 
-        print("Drawing graphs...")
-        fig = plt.figure()
-        fig.suptitle("Original WAV vs reynthesized WAV (square_2)")
-        plt.subplot(2, 1, 1)
-        plt.plot(np.arange(0, out2.size, dtype="float32"), ar2)
-        plt.grid(True)
-        plt.subplot(2, 1, 2)
-        plt.plot(np.arange(0, out2.size, dtype="float32"), out2)
-        plt.grid(True)
-        plt.show(block=False)
-
-        self.draw_diff("square_2", ar2, out2)
+        self.assertTrue(np.allclose(ar2, out))
 
 
 ################################################################################

w2b/fft.py
@@ -114,6 +114,12 @@ def get_fft_stats(n, size, overlapDec):
 ################################################################################
 def wav2bmp(fs, wav, size=1024, overlapDec=0.0, window=np.hanning):
     """Transform wave samples into a spectrogram image.
+
+    Warning: using a window in the bmp2wav flow (when recomputing the complex
+    FFT result for resynthesis with the mask image) will result in a very badly
+    scaled result!
+
+    See tests/test_bmp2wav.py.
     """
 
     if wav.ndim != 1:
@@ -201,10 +207,6 @@ def bmp2wav(fs, l, x, mask, size, overlapDec):
     that image into wave samples. This removes the need to convert the
     amplitude and angle BMPs back into complex numbers for the filter mask
     scaling.
-
-    TODO: this function does not amplify the start and end iterations
-    appropriately, according to how many times a sample was used in all
-    iterations.
     """
     assert x.ndim == 2
     assert x.ndim == mask.ndim
@@ -216,15 +218,10 @@ def bmp2wav(fs, l, x, mask, size, overlapDec):
     fftI = 0
     wavI = start
     out = np.zeros(l, dtype="float32")
-    mult = np.log2(1.0 / (1.0 - overlapDec))
-
-    if mult == 0.0:
-        mult = 1.0
-
-    print("multiple =", mult)
+    mult = size / step
 
     while wavI < l:
-        buf = irfft(x[:, fftI] * mask[:, fftI]) / mult
+        buf = irfft(x[:, fftI] * mask[:, fftI])
 
         if wavI < 0:
             bufStart = size - (size + wavI)
@@ -234,7 +231,7 @@ def bmp2wav(fs, l, x, mask, size, overlapDec):
             #print("[<] wavI =", wavI, "\tout: 0 to", wavEnd, \
             #        "\tbuf:", bufStart, "to", bufEnd, "|", wavEnd)
 
-            out[0:wavEnd] += buf[bufStart:bufEnd]
+            out[0:wavEnd] += buf[bufStart:bufEnd] / mult
         elif (wavI + size) >= l:
             bufEnd = l - wavI
             wavStart = wavI
@@ -242,7 +239,7 @@ def bmp2wav(fs, l, x, mask, size, overlapDec):
             #print("[>] wavI =", wavI, "\tout:", wavStart, "to", l, \
             #        "\tbuf: 0 to", bufEnd, "|", (l - wavStart))
 
-            out[wavStart:l] += buf[0:bufEnd]
+            out[wavStart:l] += buf[0:bufEnd] / mult
         else:
             wavStart = wavI
             wavEnd = wavStart + size
@@ -250,7 +247,7 @@ def bmp2wav(fs, l, x, mask, size, overlapDec):
             #print("[=] wavI =", wavI, "\tout:", wavStart, "to", wavEnd, \
             #        "\t\tbuf: 0 to", buf.shape[0], "|", (wavEnd - wavStart))
 
-            out[wavStart:wavEnd] += buf[:]
+            out[wavStart:wavEnd] += buf[:] / mult
 
         fftI += 1
         wavI += step